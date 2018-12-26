3661b3656292c9bf401b81ce96b17b1e8a49097f
Marvin Gaube <dev@marvingaube.de>
2018-12-26 10:11:28 +0100

    Fix wrong css class

--- src/slideshows/ffiot-de/ffiotSlideshow.vue
+++ src/slideshows/ffiot-de/ffiotSlideshow.vue
@@ -1,60 +1,61 @@
 <template lang='pug'>
-#ffiotSlideshow.eg-slideshow
-  slide
-    h1 Freifunk-IoT
-    h4 Was ist das?
-    h4 Wie geht das?
-    p @adnidor, @hibas123, @margau_me
+#ffiotSlideshow
+  .eg-slideshow
+    slide
+      h1 Freifunk-IoT
+      h4 Was ist das?
+      h4 Wie geht das?
+      p @adnidor, @hibas123, @margau_me
 
-  slide(enter='bounceInRight' leave='fadeOut')
-    h3 Motivation
-    p.
-      Come on modify me ! If you are running the development server,
-      you will see the changes take effect immediately
+    slide(enter='bounceInRight' leave='fadeOut')
+      h3 Motivation
+      p.
+        Come on modify me ! If you are running the development server,
+        you will see the changes take effect immediately
 
-  slide(enter='bounceInRight' leave='fadeOut')
-    h3 Idee
-    p.
-      Come on modify me ! If you are running the development server,
-      you will see the changes take effect immediately
+    slide(enter='bounceInRight' leave='fadeOut')
+      h3 Idee
+      p.
+        Come on modify me ! If you are running the development server,
+        you will see the changes take effect immediately
 
-  slide(enter='bounceInRight' leave='fadeOut')
-    h3 Konzept
-    p.
-      Come on modify me ! If you are running the development server,
-      you will see the changes take effect immediately
+    slide(enter='bounceInRight' leave='fadeOut')
+      h3 Konzept
+      p.
+        Come on modify me ! If you are running the development server,
+        you will see the changes take effect immediately
 
-  slide(enter='bounceInRight' leave='fadeOut')
-    h3 Module
-    p.
-      Come on modify me ! If you are running the development server,
-      you will see the changes take effect immediately
+    slide(enter='bounceInRight' leave='fadeOut')
+      h3 Module
+      p.
+        Come on modify me ! If you are running the development server,
+        you will see the changes take effect immediately
 
-  slide(enter='bounceInRight' leave='fadeOut')
-    h3 Backend
-    p.
-      Fabian, du bist dran!
+    slide(enter='bounceInRight' leave='fadeOut')
+      h3 Backend
+      p.
+        Fabian, du bist dran!
 
-  slide(enter='bounceInRight' leave='fadeOut')
-    h3 Hardware
-    p.
-      Come on modify me ! If you are running the development server,
-      you will see the changes take effect immediately
+    slide(enter='bounceInRight' leave='fadeOut')
+      h3 Hardware
+      p.
+        Come on modify me ! If you are running the development server,
+        you will see the changes take effect immediately
 
-  slide(enter='bounceInRight' leave='fadeOut')
-    h3 Ausblick
-    p.
-      Wo gehts hin?
+    slide(enter='bounceInRight' leave='fadeOut')
+      h3 Ausblick
+      p.
+        Wo gehts hin?
 
-  slide(enter='bounceInRight' leave='fadeOut')
-    h2 Mitmachen
-    ul
-      li github.com/freifunk-IoT
-      li freifunk-iot@lists.freifunk-stuttgart.de
+    slide(enter='bounceInRight' leave='fadeOut')
+      h2 Mitmachen
+      ul
+        li github.com/freifunk-IoT
+        li freifunk-iot@lists.freifunk-stuttgart.de
 
-  slide(enter='bounceInRight' leave='fadeOut')
-    h2 Vielen Dank für eure Aufmerksamkeit!
-    h3 Besucht uns an unserem Projekttisch!
+    slide(enter='bounceInRight' leave='fadeOut')
+      h2 Vielen Dank für eure Aufmerksamkeit!
+      h3 Besucht uns an unserem Projekttisch!
 </template>
 
 <script>
